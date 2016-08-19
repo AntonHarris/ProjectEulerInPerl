5b4349e175598a86097eee89ea38e5c071f22da9
AntonHarr <fourmi_007@hotmail.com>
2016-08-19 01:23:18 -0400

    Adding 040

--- 040.pl
+++ 040.pl
@@ -4,14 +4,14 @@ use strict;
 use warnings;
 
 my $str = "";
-my $strLength = 1_000_002;
+my $strLength = 1_000_000;
 my $prod = 1;
 
-for (my $i = 1 ; length $str < $strLength ; $i++) {
+for (my $i = 1 ; length $str <= $strLength ; $i++) {
 	$str = $str.$i;
 }
 
-for (my $i = 1 ; $i<$strLength ; $i*=10) {
+for (my $i = 1 ; $i<=$strLength ; $i*=10) {
 	$prod *= substr $str, ($i-1), 1;
 }
 
